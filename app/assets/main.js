c9e4ee4fa588e8cf46bad4c78f289bb2ec33fca0
Patrick Hutchinson <rphutchinson@gmail.com>
2015-06-19 13:15:29 -0400

    fixed issue with merge in main.js

--- app/assets/main.js
+++ app/assets/main.js
@@ -1,19 +1,18 @@
 angular.module('app', ['app.tpl', 'ngSanitize', 'ui.select', 'ui.bootstrap',
-  'ngRoute', 'main', 'other', 'pattern']);
+  'ngRoute', 'main', 'other', 'pattern'])
 
-app.constant('API_URL', 'replace-with-api-base-loc')
-app.config(function ($httpProvider, $locationProvider) {
+    .constant('API_URL', 'replace-with-api-base-loc')
+    .config(function ($httpProvider, $locationProvider) {
 
-  $httpProvider.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest'
-  };
+      $httpProvider.defaults.headers.common = {
+        'X-Requested-With': 'XMLHttpRequest'
+      };
 
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
-
-});
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+    });
 
 
 
