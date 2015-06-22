a79f5a82f32ccb92427b04b88b055794c979dc75
toddpress <todd.pressley1@gmail.com>
2015-06-22 10:35:00 -0400

    URI filter fix and local storage factory

--- app/assets/main/services.js
+++ app/assets/main/services.js
@@ -46,4 +46,40 @@
 
 		}
 	})
+
+	.factory('DrugsList', function($log) {
+		return {
+			all: function() {
+				var list = window.localStorage['drugslist'];
+				if (list) {
+					return angular.fromJSON(list);
+				}
+				return [];
+			},
+			save: function(drugslist) {
+				window.localStorage['drugslist'] = angular.toJSON(drugslist);
+			},
+			add: function(item) {
+				var list = this.all();
+
+				// if list doesn't contain item
+				if (!~list.indexOf(item)) list.push(item);
+				this.save(list);
+			},
+			remove: function(item) {
+				var list = this.all();
+
+				// @todo: remove console.logs like this.
+				// working - yes?
+				$log("array: ", this.all());
+
+				var index = list.indexOf(item);
+
+				// if list doesn't contain item
+				if (!~index) return;
+				list.splice(index, 1);
+				list.save();
+			}
+		}
+	})
 })();
